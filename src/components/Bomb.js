513bd3a406b3d2c3419d5d0f1d7cc37f7eb2eef5
Jexeones24 <jexeones@gmail.com>
2017-08-08 09:11:58 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,32 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+// import ImageSlider from './ImageSlider'
+
+class Bomb extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render(){
+    let text;
+    if(this.state.secondsLeft !== 0){
+      text = (
+        <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+      )
+    } else {
+      text = (
+        <p>Boom!</p>
+      )
+    }
+    return (
+      <div>{text}</div>
+    )
+  }
+}
+
+
+export default Bomb;
